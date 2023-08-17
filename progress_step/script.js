08cf20be61a48464de071c249b38c779ca7112e8
mosh-bro <connectmashiuronline@gmail.com>
2023-08-17 20:27:49 +0600

    progress_steps_update

---
+++ progress_step/script.js
@@ -0,0 +1,15 @@
+const progress = document.getElementById(progress)
+const prev = document.getElementById(prev)
+const next = document.getElementById(next)
+const circles = document.getElementById(.circles)
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.legth) {
+        currentActive = circles.legth
+    }
+    console.log(currentActive)
+})
